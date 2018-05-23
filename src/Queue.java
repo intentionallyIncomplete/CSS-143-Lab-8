7dd21c7a623847a4c38396a957bf28252e0106e4
Ian Bryan <incomplete@users.noreply.github.com>
2018-05-23 16:47:39 -0700

    Modified Queue Class to Work with LinkedList

--- src/Queue.java
+++ src/Queue.java
@@ -1,10 +1,21 @@
+import java.util.LinkedList;
 
-public class Queue {
+public class Queue<T> {
 
-	
 	
 	public static void main(String[] args){
 		
-		
+		LinkedList<Character> q = new LinkedList<Character>();
+		q.push('R');
+		q.push('a');
+		q.push('c');
+		q.push('e');
+		//a.push('c');
+		//a.push('a');
+		q.push('r');
+		System.out.println("Size : " + q.size());
+		while(!q.isEmpty()) {
+			System.out.println(q.pop());
+		}
 	}
 }
--- src/Stack.java
+++ src/Stack.java
@@ -1,9 +1,18 @@
-
-public class Stack {
+public class Stack<T>{
 
 	public static void main(String[] args) {
-		
 
+		Stack<Character> a = new Stack<Character>();
+		a.push('R');
+		a.push('a');
+		a.push('c');
+		a.push('e');
+		//a.push('c');
+		//a.push('a');
+		a.push('r');
+		System.out.println("Size : " + a.size());
+		while(!a.isEmpty()) {
+			System.out.println(a.pop());
+		}
 	}
-
 }
