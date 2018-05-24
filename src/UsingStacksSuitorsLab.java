95e5e4fe921bb579f120b87e05ea2e26b1836921
Ian Bryan <incomplete@users.noreply.github.com>
2018-05-23 19:10:04 -0700

    Added String Reversal using Stack
    Added the method functionality for reverseString using a Stack operation. The length of the string determines the size of the Stack and the string is iterated over while each pass pushes the char at the current position to the next stack frame.

--- src/UsingStacksSuitorsLab.java
+++ src/UsingStacksSuitorsLab.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 /* CSSSKL 162
  * 
@@ -27,9 +28,10 @@ public class UsingStacksSuitorsLab implements Runnable {
 		System.out.println("String1 is \"" + s1 + "\"");
 		System.out.println("String2 is \"" + s2 + "\"");
 		
-	/*	System.out.println(s1 + " reversed is: ");
+		System.out.print(s1 + " reversed is: ");
 		printReverse(s1);
-		System.out.println(s2 + " reversed is: ");
+		System.out.println();
+		System.out.print(s2 + " reversed is: ");
 		printReverse(s2);
 		
 	    recPrintReverse(s1);
@@ -37,6 +39,7 @@ public class UsingStacksSuitorsLab implements Runnable {
 		recPrintReverse(s2);
 		System.out.println();
 		
+		/*
 		System.out.println(s1 + " is a palindrome: " + isPalindrome(s1));
 		System.out.println(s2 + " is a palindrome: " + isPalindrome(s2));
 		
@@ -49,34 +52,78 @@ public class UsingStacksSuitorsLab implements Runnable {
 		System.out.println("For " + n + " suitors, stand in place:" + findPlaceToStand(n));
 		
 		n = 10;
-		System.out.println("For " + n + " suitors, stand in place:" + findPlaceToStand(n));*/
-	}
+		System.out.println("For " + n + " suitors, stand in place:" + findPlaceToStand(n));
+		*/
+	} 
 		
+	/*****************/
+	/*END MAIN METHOD*/
+	/*****************/
 	
-	
-	
+	/* *
+	*<h4>Description:</h4> 
+	*<br>
+	*<p>
+	* 
+	*</p>
+	*@param
+	*@see
+	* */
 	public static void printReverse(String target) {
-		//todo: use a stack
+		//using Stack
+		Stack<Character> reverseString = new Stack<Character>();
+		
+		for(int i=0;i<target.length();i++){
+			reverseString.push(target.charAt(i));
+			//System.out.println("pushed: " + target.charAt(i) + " to next stack frame");
+		}
+		
+		while(!reverseString.isEmpty()){
+			System.out.print(reverseString.pop() + " ");
+		}
 	}
 	
+	/*
+	 * 
+	 * 
+	 * */
 	public static void recPrintReverse(String target) {
 		//todo
 	}
 	
+	/*
+	 * 
+	 * 
+	 * 
+	 * */
 	public static boolean isPalindrome(String input) {
 		//todo: use a stack
 	}
 
+	/*
+	 * 
+	 * 
+	 * 
+	 * */
 	public static boolean isPalindromeRec(String sentence)	{
 	  	//todo
 	}
 	
+	/*
+	 * 
+	 * 
+	 * */
 	public static int findPlaceToStand(int numSuitors) {
 		//todo
 		return -1;
 	}	
 
-
+	/*
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
 	public static boolean buildThreadQueue() {	//returns true upon success
 		Queue<Thread> q = new LinkedList<Thread>(); //comment this out and use your own Queue
 		
